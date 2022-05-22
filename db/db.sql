bb2504771b10f7cac473e603788bf52c26c821f6
Dev <devinlgar@gmail.com>
2022-05-22 13:23:35 -0400

    initial files, dependencies $ schema tables

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS eManager;
+CREATE DATABASE eManager;
+USE eManager;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const db = require('./db/connection');
+const inquirer = require('inquirer');
+const table = require('./db/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
